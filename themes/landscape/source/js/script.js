7d09f5554693545abeb531e1457b6b824d38343e
nerd barbarous <473112889@qq.com>
2022-08-29 12:14:22 +0800

    乱七八糟的尝试

--- themes/landscape/source/js/script.js
+++ themes/landscape/source/js/script.js
@@ -134,4 +134,18 @@
 
     $container.removeClass('mobile-nav-on');
   });
+
+
+  let language = {};
+  language.now = location.pathname.match(/^\/en/) ? 'en' : 'cn';
+  if('en' === language.now){
+    language.label = '中文';
+    language.href = location.pathname.replace(/^\/en/, '/cn');
+  }else{
+    language.label = 'English';
+    language.href = location.pathname.replace(/^\/cn/, '/en');
+  }
+  $('#sub-nav').prepend(`<a class="main-nav-link" href="${language.href}">${language.label}</a>`)
+
+  
 })(jQuery);
